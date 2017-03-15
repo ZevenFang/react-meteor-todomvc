c2da3cc8eff532078cfb31dbfe2fd76b017c3f07
zeven <fangfan1997@126.com>
2017-03-15 20:15:56 +0800

    use HashRouter

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter,
   Route
 } from 'react-router-dom'
 import IndexPage from './routes/IndexPage';
@@ -9,12 +9,12 @@ import RegisterPage from './routes/RegisterPage';
 
 export default function() {
   return (
-    <Router>
+    <HashRouter>
       <div>
         <Route exact path="/" component={IndexPage} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/login" component={LoginPage} />
       </div>
-    </Router>
+    </HashRouter>
   );
 };
